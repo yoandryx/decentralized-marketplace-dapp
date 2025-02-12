a8632fb1ab759580cc897d2803b5e4b9801471b1
yoandryx <yoandrycontact@gmail.com>
2025-02-12 11:12:26 -0500

    feat: add wallet connect button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,24 @@
 import type { AppProps } from "next/app";
+import { useMemo } from "react";
+import { WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+// import default styles for the wallet modal
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    //create a memoized list of wallets to use.
+    const wallets = useMemo(() => [
+        new PhantomWalletAdapter, 
+        new SolflareWalletAdapter
+    ], []);
+
+    return (
+        <WalletProvider wallets = {wallets} autoConnect>
+            <WalletModalProvider>
+                <Component {...pageProps} />
+            </WalletModalProvider>
+        </WalletProvider>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+const WalletMultiButtonDynamic = dynamic(() => import("@solana/wallet-adapter-react-ui").then((mod) => mod.WalletMultiButton), { ssr: false });
+
 export default function Home() {
     return (
-      <div>
-        <h1>Welcome to the Decentralized Marketplace</h1>
-        <p>Built on Solana with Next.js and Tailwind CSS</p>
-      </div>
+        <div className="min-h-scren flex flex-col items-center justify-center bg-gray-100">
+
+            <h1 className="text-3xl font-bold mb-4">
+                Welcome too the Decentralized Marketplace
+            </h1>
+
+            <p className="mb-4">
+                Built on Solana with Next.js, Tailwind CSS, and Wallet Adapter.
+            </p>
+
+            {/* Use the dynamically imported wallet button */}
+            <WalletMultiButtonDynamic />
+            
+        </div>
     );
   }
